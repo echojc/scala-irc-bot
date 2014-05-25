3932df0276453353ce93dbe1aa5d423ef3d96b83
Jonathan Chow <jonathan@movio.co>
2014-05-25 17:24:18 +1200

    Adds independent logger to EchoBot.

--- echobot/src/main/scala/sh/echo/echobot/EchoBot.scala
+++ echobot/src/main/scala/sh/echo/echobot/EchoBot.scala
@@ -5,25 +5,31 @@ import scala.util.Success
 
 import net.mtgto.irc._
 import net.mtgto.irc.event._
+import org.slf4j.LoggerFactory
 
 class EchoBot extends Bot {
+  val logger = LoggerFactory.getLogger(getClass)
 
   val musicService = new MusicService()
   import musicService.system.dispatcher
 
   def fail(client: Client, channel: String, e: Throwable): Unit = {
-    client.sendMessage(channel, s"Request failed: [$e]")
+    logger.error(s"failed request: [$e]")
+    client.sendMessage(channel, s"Request failed: [${e.getMessage}]")
   }
 
   override def onMessage(client: Client, message: Message) = {
-    client.logger.info(s"got message [$message]")
+    logger.debug(s"got message [$message]")
 
     val Message(channel, _, _, _, text, _) = message
     client.sendMessage(channel, s"""sending GET request for "$text"""")
 
     musicService.vanillaGet(text) andThen {
-      case Success(response) ⇒ client.sendMessage(channel, response)
-      case Failure(e) ⇒ fail(client, channel, e)
+      case Success(response) ⇒
+        logger.debug(s"vanillaGet request for [$text] succeeded")
+        client.sendMessage(channel, response)
+      case Failure(e) ⇒
+        fail(client, channel, e)
     }
   }
 
