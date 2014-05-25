e7a84ff4880fe6f2730f703761d43ca8f98a79ae
Jonathan Chow <jonathan@movio.co>
2014-05-25 17:54:50 +1200

    Adds a default system bot.

---
+++ src/main/scala/sh/echo/SystemBot.scala
@@ -0,0 +1,37 @@
+package sh.echo
+
+import com.typesafe.config.Config
+import net.mtgto.irc._
+import net.mtgto.irc.event._
+import org.slf4j.LoggerFactory
+
+class SystemBot(config: Config) extends Bot {
+  val logger = LoggerFactory.getLogger(getClass)
+
+  val isShutdownEnabled = config.getBoolean("shutdown-enabled")
+  val commandPrefix = config.getString("command-prefix")
+
+  override def onMessage(client: Client, message: Message) = {
+    val Message(channel, _, _, _, text, _) = message
+    if (text startsWith (commandPrefix)) {
+      logger.debug(s"got command: [$text]")
+
+      val command = text.split(' ').head.drop(commandPrefix.length)
+      command match {
+        case "reload" ⇒
+          logger.info("reloading config and bots")
+          client.sendMessage(channel, "reloading config and bots...")
+        case "shutdown" if isShutdownEnabled ⇒
+          logger.info("shutting down!")
+          client.sendMessage(channel, "I am shutting down!")
+          client.disconnect
+          System.exit(0)
+        case "shutdown" if !isShutdownEnabled ⇒
+          logger.info("shutdown triggered but is disabled")
+          client.sendMessage(channel, "Shutdown is disabled!")
+        case _ ⇒
+          logger.debug(s"got unknown command [$command]")
+      }
+    }
+  }
+}
